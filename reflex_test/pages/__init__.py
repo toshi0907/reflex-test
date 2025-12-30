b591bc5972418be2fcc75f974cf6e1e9fc84215c
toshi0907 <n00toshi@gmail.com>
2025-12-30 08:19:29 +0000

    ブックマークページを追加し、アプリに統合。共通ヘッダにブックマークリンクを追加。

--- reflex_test/pages/__init__.py
+++ reflex_test/pages/__init__.py
@@ -2,5 +2,6 @@
 
 from .index import index
 from .todo_page import todo_page
+from .bookmark_page import bookmark_page
 
-__all__ = ["index", "todo_page"]
+__all__ = ["index", "todo_page", "bookmark_page"]
