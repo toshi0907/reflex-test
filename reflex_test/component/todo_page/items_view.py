e7c23ef88d820f6e0bf91a0e35e946c499ba0859
toshi0907 <n00toshi@gmail.com>
2025-12-30 03:10:06 +0000

    Todoアイテムの説明を改良し、複数行表示に対応

--- reflex_test/component/todo_page/items_view.py
+++ reflex_test/component/todo_page/items_view.py
@@ -47,11 +47,16 @@ def todo_page_view_items() -> rx.Component:
                 ),
                 rx.cond(
                     (item.description != "") & (item.description is not None),
-                    rx.text_area(
-                        f"{item.description}",
-                        is_read_only=True,
-                        width="100%",
-                        minwidth="300px",
+                    rx.foreach(
+                        item.description.split("\n"),
+                        lambda line: rx.text(
+                            f"{line}",
+                            margin_left="15px",
+                            padding_top="0px",
+                            padding_bottom="0px",
+                            margin_top="0px",
+                            margin_bottom="0px",
+                        ),
                     ),
                     None,
                 ),
