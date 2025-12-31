5b68c54265567ebb6dd78437f210c0f49dbe7cfc
toshi0907 <n00toshi@gmail.com>
2025-12-31 05:29:45 +0000

    ブックマークページにカテゴリ登録フォームと表示機能を追加

--- reflex_test/component/bookmark_page/__init__.py
+++ reflex_test/component/bookmark_page/__init__.py
@@ -1,6 +1,6 @@
 """bookmark page components"""
 
-from .regist_form import bookmark_page_regist_item
+from .regist_form import bookmark_page_regist_item, bookmark_category_regist_item
 from .items_view import bookmark_page_view_items
 
-__all__ = ["bookmark_page_regist_item", "bookmark_page_view_items"]
+__all__ = ["bookmark_page_regist_item", "bookmark_page_view_items", "bookmark_category_regist_item"]
--- reflex_test/pages/bookmark_page.py
+++ reflex_test/pages/bookmark_page.py
@@ -4,6 +4,7 @@ import reflex as rx
 import reflex_test.pages.common as common
 from reflex_test.component.bookmark_page import (
     bookmark_page_regist_item,
+    bookmark_category_regist_item,
     bookmark_page_view_items,
 )
 
@@ -15,6 +16,10 @@ def bookmark_page() -> rx.Component:
     return rx.container(
         common.CommonHeader(title="Bookmark"),
         bookmark_page_regist_item(),
+        rx.spacer(height="20px"),
+        rx.divider(),
+        rx.spacer(height="20px"),
+        bookmark_category_regist_item(),
         rx.spacer(height="20px"),
         rx.divider(),
         rx.spacer(height="20px"),
