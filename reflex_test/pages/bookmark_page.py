e04ce51deecef88c0d499c63661eaa610d2f5764
toshi0907 <n00toshi@gmail.com>
2025-12-31 02:35:18 +0000

    ブックマークページの表示モック

--- reflex_test/pages/bookmark_page.py
+++ reflex_test/pages/bookmark_page.py
@@ -2,12 +2,26 @@
 
 import reflex as rx
 import reflex_test.pages.common as common
+from reflex_test.component.bookmark_page import (
+    bookmark_page_regist_item,
+    bookmark_page_view_items,
+)
+
+# from reflex_test.states import StateTodo # TODO
 
 
 def bookmark_page() -> rx.Component:
     """Bookmarkページ"""
     return rx.container(
         common.CommonHeader(title="Bookmark"),
+        bookmark_page_regist_item(),
+        rx.spacer(height="20px"),
+        rx.divider(),
+        rx.spacer(height="20px"),
+        bookmark_page_view_items(),
+        rx.spacer(height="20px"),
+        rx.divider(),
+        rx.spacer(height="20px"),
         minwidth="300px",
         width="100%",
     )
