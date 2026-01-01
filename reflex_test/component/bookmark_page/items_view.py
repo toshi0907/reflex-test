5888ea37a84c0a519d8fb4b2e73f24db51dbb6c2
toshi0907 <n00toshi@gmail.com>
2026-01-01 22:26:42 +0000

    ブックマークアイテムの表示実装

--- reflex_test/component/bookmark_page/items_view.py
+++ reflex_test/component/bookmark_page/items_view.py
@@ -1,18 +1,47 @@
 """bookmarkアイテム表示コンポーネント"""
 
 import reflex as rx
+from reflex_test.states import (
+    StateBookmark,
+    StateBookmarkCategory,
+)
 
 
 def bookmark_page_view_items() -> rx.Component:
     """bookmarkアイテム一覧表示"""
     return rx.vstack(
         rx.heading("Bookmark Items", as_="h2"),
-        rx.heading("Category1", as_="h3"),
-        rx.link("google", href="https://www.google.com", is_external=True),
-        rx.link("yahoo", href="https://www.yahoo.co.jp", is_external=True),
-        rx.heading("Category2", as_="h3"),
-        rx.link("google", href="https://www.google.com", is_external=True),
-        rx.link("yahoo", href="https://www.yahoo.co.jp", is_external=True),
+        rx.foreach(
+            StateBookmark.dbitemsCategory,
+            lambda item_cate: rx.vstack(
+                rx.heading(f"{item_cate.category_name}", as_="h3"),
+                rx.foreach(
+                    StateBookmark.dbitems,
+                    lambda item: rx.cond(
+                        item_cate.id == item.category_id,
+                        rx.vstack(
+                            rx.hstack(
+                                rx.link(
+                                    f"・{item.category_id} : {item.title}",
+                                    href=item.url,
+                                    is_external=True,
+                                ),
+                            ),
+                            rx.cond(
+                                (item.description != "")
+                                & (item.description is not None),
+                                rx.foreach(
+                                    item.description.split("\n"),
+                                    lambda line: rx.text(f"des:{line}"),
+                                ),
+                                None,
+                            ),
+                        ),
+                        None,
+                    ),
+                ),
+            ),
+        ),
         width="100%",
         minwidth="300px",
     )
