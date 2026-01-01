be5024f8bbb8c1223e15a6caed2316fd2b74dbf0
toshi0907 <n00toshi@gmail.com>
2026-01-01 22:51:35 +0000

    ブックマークアイテム表示をアコーディオン形式に変更

--- reflex_test/component/bookmark_page/items_view.py
+++ reflex_test/component/bookmark_page/items_view.py
@@ -11,36 +11,42 @@ def bookmark_page_view_items() -> rx.Component:
     """bookmarkアイテム一覧表示"""
     return rx.vstack(
         rx.heading("Bookmark Items", as_="h2"),
-        rx.foreach(
-            StateBookmark.dbitemsCategory,
-            lambda item_cate: rx.vstack(
-                rx.heading(f"{item_cate.category_name}", as_="h3"),
-                rx.foreach(
-                    StateBookmark.dbitems,
-                    lambda item: rx.cond(
-                        item_cate.id == item.category_id,
-                        rx.vstack(
-                            rx.hstack(
-                                rx.link(
-                                    f"・{item.category_id} : {item.title}",
-                                    href=item.url,
-                                    is_external=True,
+        rx.accordion.root(
+            rx.foreach(
+                StateBookmark.dbitemsCategory,
+                lambda item_cate: rx.accordion.item(
+                    header=f"{item_cate.category_name}",
+                    content=rx.foreach(
+                        StateBookmark.dbitems,
+                        lambda item: rx.cond(
+                            item_cate.id == item.category_id,
+                            rx.vstack(
+                                rx.hstack(
+                                    rx.link(
+                                        f"・{item.category_id} : {item.title}",
+                                        href=item.url,
+                                        is_external=True,
+                                    ),
+                                ),
+                                rx.cond(
+                                    (item.description != "")
+                                    & (item.description is not None),
+                                    rx.foreach(
+                                        item.description.split("\n"),
+                                        lambda line: rx.text(f"des:{line}"),
+                                    ),
+                                    None,
                                 ),
                             ),
-                            rx.cond(
-                                (item.description != "")
-                                & (item.description is not None),
-                                rx.foreach(
-                                    item.description.split("\n"),
-                                    lambda line: rx.text(f"des:{line}"),
-                                ),
-                                None,
-                            ),
+                            None,
                         ),
-                        None,
                     ),
+                    value=f"item_{item_cate.id}",
                 ),
             ),
+            collapsible=True,
+            type="single",
+            color_scheme="gray",
         ),
         width="100%",
         minwidth="300px",
