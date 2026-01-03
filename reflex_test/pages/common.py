abdef7fd7cdb2f923a0007e62cf50b37dd949deb
toshi0907 <n00toshi@gmail.com>
2026-01-03 09:24:59 +0000

    .env.exampleにデバッグモード設定を追加し、CommonHeader関数でデバッグ情報を表示するように修正

--- reflex_test/pages/common.py
+++ reflex_test/pages/common.py
@@ -1,17 +1,30 @@
 """共通コンポーネント集"""
 
 import reflex as rx
+import os
+from dotenv import load_dotenv
+
+# .envファイルを読み込み
+load_dotenv()
 
 
 def CommonHeader(title: str) -> rx.Component:
     """共通ヘッダ"""
     title_disp = ""
+    title_debug = ""
+    if os.getenv("TN_DEBUG") == "True":
+        title_debug = "[DEBUG] "
     if title == "":
-        title_disp = "TN App"
+        title_disp = title_debug + "TN App"
     else:
-        title_disp = "TN App : " + title
+        title_disp = title_debug + "TN App : " + title
     return rx.vstack(
-        rx.heading(title_disp, as_="h1"),
+        rx.heading(
+            title_disp,
+            trim="both",
+            as_="h1",
+            color_scheme="red",
+        ),
         rx.hstack(
             rx.badge(rx.link("Top", href="/")),
             rx.badge(rx.link("Todo", href="/todo_page")),
