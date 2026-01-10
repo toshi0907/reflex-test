1ddefe933fead760a54ecc67ed5aa363bb4c576f
toshi0907 <n00toshi@gmail.com>
2026-01-10 13:16:39 +0000

    スマホ情報関連のデータベースモデルを新規作成

---
+++ reflex_test/models/phone.py
@@ -0,0 +1,20 @@
+"""スマホ情報関連のデータベースモデル"""
+
+import reflex as rx
+from typing import Optional
+
+
+class DBSmartPhoneInfoItems(rx.Model, table=True):
+    """データベースのテーブル定義 : スマホ情報"""
+
+    create_at: str
+
+    ### 位置情報
+    latitude: Optional[float]  # 緯度
+    longitude: Optional[float]  # 経度
+
+    ### バッテリー情報
+    battery_level: Optional[float]  # バッテリー残量(0.0~1.0)
+
+    ### ストレージ情報
+    free_storage: Optional[int]  # 空きストレージ容量(GB)
